d51c03158f015cd5d27421338e395d3448503ecc
sas <2838576429@qq.com>
2024-09-22 14:04:40 +0800

    http连接池化

--- dcloud-common/src/main/java/work/etasas/config/RestTemplateConfig.java
+++ dcloud-common/src/main/java/work/etasas/config/RestTemplateConfig.java
@@ -1,11 +1,21 @@
 package work.etasas.config;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
 
 /**
  * @Title: RestTemplateConfig
@@ -23,9 +33,38 @@ public class RestTemplateConfig {
 
     @Bean
     public ClientHttpRequestFactory clientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(10000);
-        factory.setReadTimeout(10000);
-        return factory;
+        return new HttpComponentsClientHttpRequestFactory(httpClient());
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+
+        //设置连接池最大是500连接
+        connectionManager.setMaxTotal(500);
+
+        //设置每个主机的并发
+        connectionManager.setDefaultMaxPerRoute(300);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                //返回数据的超时时间
+                .setSocketTimeout(10000)
+                //连接上服务器的时间
+                .setConnectTimeout(10000)
+                //从连接池中获取连接的超时时间
+                .setConnectionRequestTimeout(10000)
+                .build();
+
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return closeableHttpClient;
     }
 }
