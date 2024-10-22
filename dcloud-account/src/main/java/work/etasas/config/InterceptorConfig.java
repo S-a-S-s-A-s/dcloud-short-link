c58f9552e782000702006d898e1142ddcd26e864
sas <2838576429@qq.com>
2024-10-22 09:11:00 +0800

    拦截器配置

---
+++ dcloud-account/src/main/java/work/etasas/config/InterceptorConfig.java
@@ -0,0 +1,23 @@
+package work.etasas.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import work.etasas.interceptor.LoginInterceptor;
+
+/**
+ * @author sas
+ * @create 2024-10-22-9:01
+ */
+@Configuration
+@Slf4j
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/api/account/*/**", "/api/traffic/*/**")
+                .excludePathPatterns
+                        ("/api/account/*/login", "/api/account/*/register", "/api/account/*/upload", "/api/notify/*/captcha", "/api/notify/*/send_code");
+    }
+}
